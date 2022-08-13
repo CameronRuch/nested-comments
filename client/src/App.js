59790eb57ee98fd941c376254362c542a3ac12f9
CameronRuch <cameron.ruch@yahoo.com>
2022-08-13 01:04:29 -0500

    working application

---
+++ client/src/App.js
@@ -0,0 +1,25 @@
+import { Routes, Route } from 'react-router-dom'
+import { PostProvider } from "./contexts/PostContext";
+import { Post } from "./components/Post";
+import { PostLists } from "./components/PostLists";
+
+function App() {
+  return (
+    <div className="container">
+      <Routes>
+        <Route path="/" element={<PostLists />} />
+        <Route
+          path="/posts/:id"
+          element={
+            <PostProvider>
+              <Post />
+            </PostProvider>
+          }
+        />
+      </Routes>
+    </div>
+  )
+}
+
+
+export default App;
